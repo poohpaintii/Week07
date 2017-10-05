9324fbb1d11d63cfebbc41085c3654b49ceea322
poohpaintii <poohpaintii@gmail.com>
2017-10-05 23:48:51 +0700

    SS

---
+++ Lab07/Lab07/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Teacher t = new Person();
+            t.SayHi();
+            Console.ReadKey();
+        }
+
+
+
+        class Person
+        {
+            public virtual void SayHi()
+            {
+                Console.WriteLine("Hi I'm a person");
+            }
+        }
+        class Student : Person
+        {
+            public override void SayHi()
+            {
+                Console.WriteLine("Hi I'm a student");
+            }
+        }
+        class Teacher : Person
+        {
+            public override void SayHi()
+            {
+                Console.WriteLine("Hi I'm a teacher");
+            }
+        }
+        class Professor : Teacher
+        {
+            public new void SayHi()
+            {
+                Console.WriteLine("Hi I'm a Professor");
+            }
+        }
+
+    }
+}
+
+
+
